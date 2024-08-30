6f29a33dc38d85952cee8ff9dccad6809d81e721
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-08-31 00:28:30 +1000

    chore: initial server project setup

---
+++ server/src/main/java/io/marutsuki/asmallworld/AsmallworldApplication.java
@@ -0,0 +1,13 @@
+package io.marutsuki.asmallworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmallworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsmallworldApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/io/marutsuki/asmallworld/AsmallworldApplicationTests.java
@@ -0,0 +1,13 @@
+package io.marutsuki.asmallworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmallworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
